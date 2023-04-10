d6937c9d72584bfe240c8e526da1a1bcec4b594d
sachishrivastava22 <127180412+sachishrivastava22@users.noreply.github.com>
2023-04-10 07:57:17 +0000

    main

---
+++ introductory_exercise_2.PY
@@ -0,0 +1,11 @@
+"""
+Conversion: Input some numbers, do some simple arithmetic to do silly conversions(Python3)
+"""
+a=float(input())
+b=float(input())
+
+feet=a*10
+pound=b*12
+
+print(feet)
+print(pound)
