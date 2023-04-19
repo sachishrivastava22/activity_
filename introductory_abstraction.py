31c5b2e7265470fb4a6f6153c8b54b06b399b96a
sachishrivastava22 <127180412+sachishrivastava22@users.noreply.github.com>
2023-04-19 08:49:45 +0000

    main

---
+++ introductory_abstraction.py
@@ -0,0 +1,10 @@
+'''Arithmetic: Input some numbers, do some simple arithmetic, output results (Python3)'''
+
+def do_arithmetic(a, b):
+    addition = a + b
+    subtraction = a - b
+    multiplication = a * b
+    division = a / b
+    modulus = a % b
+
+    return addition, subtraction, multiplication, division, modulus
