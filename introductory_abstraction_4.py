7d43c37cd4ab86a43883033cadc304693e95391d
sachishrivastava22 <127180412+sachishrivastava22@users.noreply.github.com>
2023-04-20 16:49:24 +0000

    main

--- introductory_abstraction_4.py
+++
@@ -1,8 +0,0 @@
-'''Factor:  Calculate temperature that a person feels because of the wind (Python3)'''
-import math
-
-def calculate_wind_chill_factor(wind_speed, temperature):
-    # formula to calculate wind chill factor
-    factor = 35.74 + 0.6215*temperature - 35.75*math.pow(wind_speed, 0.16) + 0.4275*temperature*math.pow(wind_speed, 0.16)
-
-    return f"The wind chill factor is {factor} degrees Fahrenheit."
