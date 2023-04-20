b4dad2ed9ad3b0892f44aa35f268dd88ff6f16de
sachishrivastava22 <127180412+sachishrivastava22@users.noreply.github.com>
2023-04-20 16:48:46 +0000

    main

--- introductory_abstraction_3.py
+++
@@ -1,8 +0,0 @@
-'''Earthquake Impact: Input some numbers, do some simple arithmetic, output results. (Python3)'''
-
-def calculate_earthquake_impact(magnitude):
-    # formula to calculate impact based on magnitude
-    impact = 10**(1.5*magnitude - 5.75)
-
-    return f"The potential impact of the earthquake is {impact}"
-
